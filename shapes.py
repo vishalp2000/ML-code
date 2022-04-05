e734f7fcb220b4901c1b9c1ea21dcd261c515ded
Danderson465 <103059973+Danderson465@users.noreply.github.com>
2022-04-05 12:40:50 -0400

    I added a comment

--- shapes.py
+++ shapes.py
@@ -9,7 +9,7 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Compose
 import matplotlib.pyplot as plot
 
-import time
+import time #This is time
 from time import time as now
 import torch.nn.functional as F
 import torch.optim as optim
